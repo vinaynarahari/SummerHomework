4f3e7d5961091f847ebd6f6785a3e0d54cc3969a
vnarahari <111399243+vnarahari@users.noreply.github.com>
2022-08-16 22:34:54 -0500

    Update and rename fahrenheitCelsius.java to twoPointFour.java

--- twoPointFour.java
+++ twoPointFour.java
@@ -5,7 +5,7 @@ import java.util.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class fahrenheitCelsius
+public class twoPointFour
 {
     public static void main (String[] args){
         
