f20bdb42b35167f97362f06fdb46761d9ef16255
vnarahari <111399243+vnarahari@users.noreply.github.com>
2022-08-16 22:26:34 -0500

    Update and rename 2.2.java to twopointtwo.java

--- twopointtwo.java
+++ twopointtwo.java
@@ -5,7 +5,7 @@ import java.util.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class 2.2
+public class twopointtwo
 {
     public static void main (String[] args){
         
